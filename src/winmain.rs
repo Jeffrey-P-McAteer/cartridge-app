9bb6afd48c37521230e8b0e79a7e3b119a4d755b
jmcateer <jmcateer@cs.odu.edu>
2019-02-08 17:45:23 -0500

    untested windows code

--- src/winmain.rs
+++ src/winmain.rs
@@ -1,7 +1,32 @@
 
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn os_main() {
+  // Take embedded SDL2.dll and put it in current dir
+  {
+    let mut file = File::create("SDL2.dll").expect("Could not create SDL2.dll");
+    // Write a slice of bytes to the file
+    match file.write_all(include_bytes!("SDL2.dll")) {
+      Ok(_) => { }
+      Err(e) => {
+        println!("{}", e);
+      }
+    }
+  }
+  
   // Windows doesn't exactly have a stable temp file API
   // and I'm not going to invent one, we'll just dump the icon wherever we currently are.
+  {
+    let mut file = File::create("icon.png").expect("Could not create icon.png");
+    // Write a slice of bytes to the file
+    match file.write_all(include_bytes!("icon.png")) {
+      Ok(_) => { }
+      Err(e) => {
+        println!("{}", e);
+      }
+    }
+  }
   crate::make_tray( "icon.png".to_string() );
 }
 
